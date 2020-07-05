454de58261fdd6ac6e050b761ea93e8001524e54
Masayoshi Iraha <urasho6.12@gmail.com>
2020-07-05 19:33:25 +0900

    add PlayerMovement
    add PlayerMovement

---
+++ ShootingGameProject/Assets/Scripts/Missile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += new Vector3(0, 8f, 0) * Time.deltaTime;
+        
+    }
+}
---
+++ ShootingGameProject/Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+
+    // ミサイルの発車位置
+    public Transform firePoint; 
+    public GameObject missilePrefab;
+
+    public float playerSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        PlayerShot();
+
+    }
+
+    private void PlayerShot()
+    {
+
+        // Playerのミサイルを生成
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missilePrefab, firePoint.position, transform.rotation);
+        }
+    }
+
+    private void PlayerMovement()
+    {
+        // Playerの動き
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x, y, 0) * Time.deltaTime * playerSpeed;
+    }
+}
